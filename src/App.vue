b278775b8365282dc0db3453e4f6a50f265f680d
bymoen <alexb@innotechcollege.com>
2023-04-18 19:36:01 -0600

    communicating

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ username }}</h1>
+    <small-user-profile
+      v-for="(user, i) in users"
+      :key="i"
+      :user="users[i]"
+      @user_clicked="handle_user_clicked"
+    >
+    </small-user-profile>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SmallUserProfile from "@/components/SmallUserProfile.vue";
 
 export default {
-  name: 'App',
+  methods: {
+    handle_user_clicked: function (name) {
+      this.username = name;
+    },
+  },
+  data() {
+    return {
+      username: undefined,
+      users: [
+        {
+          name: `Bob User`,
+          age: 27,
+          is_premium: true,
+          id: 16678,
+        },
+        {
+          name: `Alice Admin`,
+          age: 34,
+          is_premium: false,
+          id: 42,
+        },
+      ],
+    };
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SmallUserProfile,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/SmallUserProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>{{ user[`name`] }}</h3>
+    <p>{{ user[`age`] }}</p>
+    <h5 v-if="user[`is_premium`] === true">PREMIUM</h5>
+    <p v-else>Basic</p>
+    <button @click="user_clicked">CLICK ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    user_clicked() {
+      this.$emit(`user_clicked`, this.user[`name`]);
+    },
+  },
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style scoped>
+</style>
